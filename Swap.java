e583181016fa623c1e08bf08a9dc8913e066b5d9
Mounika <nagargojemounika@gmail.com>
2025-01-28 21:51:02 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,14 @@
+public class Swap {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c;
+        System.out.printf("Before swapping a=%d, b=%d",a,b);
+        c = a;
+        a = b;
+        b = c;
+        System.out.printf("\nAfter swapping a=%d, b=%d",a,b);
+
+    }
+    
+}
